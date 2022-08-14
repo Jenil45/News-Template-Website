aadc6d438d58eba122a2a847b8503606f3e822c3
Jenil45 <103255558+Jenil45@users.noreply.github.com>
2022-08-14 20:24:14 +0530

    Update add-category.php

--- admin/add-category.php
+++ admin/add-category.php
@@ -1,4 +1,26 @@
-<?php include "header.php"; ?>
+<?php include "header.php";
+
+    include "connect.php";
+    if(isset($_POST['save']))
+    {
+        $cat = mysqli_real_escape_string($connection , $_POST['cat']);
+        $sql = "SELECT category_name FROM category WHERE category_name='$cat'";
+        $result = mysqli_query($connection , $sql) or die("Query failed");
+
+        if(mysqli_num_rows($result) > 0){
+            echo "<div class='alert alert-danger' role='alert'>
+            Category Already Exist
+          </div>";
+        }
+        else
+        {
+            $sql1 = "INSERT INTO category (category_name , post) VALUES ('{$cat}' , 0 )";
+            if (mysqli_query($connection , $sql1)) {
+                header("Location: http://localhost/news-template/admin/category.php");
+            }
+        }
+    }
+?>
   <div id="admin-content">
       <div class="container">
           <div class="row">
@@ -7,7 +29,7 @@
               </div>
               <div class="col-md-offset-3 col-md-6">
                   <!-- Form Start -->
-                  <form action="" method="POST" autocomplete="off">
+                  <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" autocomplete="off">
                       <div class="form-group">
                           <label>Category Name</label>
                           <input type="text" name="cat" class="form-control" placeholder="Category Name" required>
