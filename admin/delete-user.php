b512d5fc9b5c518debbd51a427c1d2f01a1f85bb
Jenil45 <103255558+Jenil45@users.noreply.github.com>
2022-08-16 21:37:26 +0530

    Update delete-user.php

--- admin/delete-user.php
+++ admin/delete-user.php
@@ -1,5 +1,9 @@
 <?php
-
+    if($_SESSION['role'] == 0)
+    {
+        header("Location: http://localhost/news-template/admin/post.php");
+    }
+    
     include 'connect.php';
     $id = $_GET['id'];
 
